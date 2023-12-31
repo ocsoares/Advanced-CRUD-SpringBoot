1f22130d6094f4e5cbbc0a39402f5499e74d9bb2
ocsoares <casoares016@gmail.com>
2023-12-31 19:57:36 -0300

    refactor: rename main package

--- src/main/java/com/ocsoares/advancedcrudspringboot/AdvancedCrudSpringBootApplication.java
+++ src/main/java/com/ocsoares/advancedcrudspringboot/AdvancedCrudSpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.ocsoares.AdvancedCRUDSpringBoot;
+package com.ocsoares.advancedcrudspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdvancedCrudSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdvancedCrudSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedCrudSpringBootApplication.class, args);
+    }
 
 }
