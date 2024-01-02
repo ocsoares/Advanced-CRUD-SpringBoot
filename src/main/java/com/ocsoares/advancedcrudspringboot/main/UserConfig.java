f61a43ad01b11cf33b11e3a2aa493e1e62184db4
ocsoares <casoares016@gmail.com>
2024-01-02 20:02:28 -0300

    feat: add abstract PasswordHasher to UserConfig

--- src/main/java/com/ocsoares/advancedcrudspringboot/main/UserConfig.java
+++ src/main/java/com/ocsoares/advancedcrudspringboot/main/UserConfig.java
@@ -1,37 +1,45 @@
 package com.ocsoares.advancedcrudspringboot.main;
 
+import com.ocsoares.advancedcrudspringboot.application.gateways.security.PasswordHasherGateway;
 import com.ocsoares.advancedcrudspringboot.application.gateways.user.IUserGateway;
 import com.ocsoares.advancedcrudspringboot.application.usecases.user.CreateUserUseCase;
 import com.ocsoares.advancedcrudspringboot.infrastructure.controllers.user.mapper.UserDTOMapper;
+import com.ocsoares.advancedcrudspringboot.infrastructure.gateways.security.bcrypt.BcryptHasher;
 import com.ocsoares.advancedcrudspringboot.infrastructure.gateways.user.jpa.JpaUserRepositoryGateway;
 import com.ocsoares.advancedcrudspringboot.infrastructure.mappers.UserPersistenceEntityMapper;
 import com.ocsoares.advancedcrudspringboot.infrastructure.persistence.repository.jpa.JpaUserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 // Camada para aplicar a Injeção de Dependência, e estou usando o Spring Boot, mas pode ser facilmente
 // TROCADO por outra Lógica ou Framework, separando da REGRA de NEGÓCIO da Aplicação!!!
 @Configuration
 public class UserConfig {
     @Bean
-    CreateUserUseCase createUserUseCase(IUserGateway userGateway) {
-        return new CreateUserUseCase(userGateway);
+    public CreateUserUseCase createUserUseCase(IUserGateway userGateway, PasswordHasherGateway passwordHasherGateway) {
+        return new CreateUserUseCase(userGateway, passwordHasherGateway);
     }
 
     @Bean
-    IUserGateway userGateway(
+    public IUserGateway userGateway(
             JpaUserRepository jpaUserRepository, UserPersistenceEntityMapper userPersistenceEntityMapper
     ) {
         return new JpaUserRepositoryGateway(jpaUserRepository, userPersistenceEntityMapper);
     }
 
     @Bean
-    UserPersistenceEntityMapper userPersistenceEntityMapper() {
+    public PasswordHasherGateway passwordHasherGateway(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        return new BcryptHasher(bCryptPasswordEncoder);
+    }
+
+    @Bean
+    public UserPersistenceEntityMapper userPersistenceEntityMapper() {
         return new UserPersistenceEntityMapper();
     }
 
     @Bean
-    UserDTOMapper userDTOMapper() {
+    public UserDTOMapper userDTOMapper() {
         return new UserDTOMapper();
     }
 }
