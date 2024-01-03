ddb7e7ac4837c674d9d96eb7dda7884090195b3a
ocsoares <casoares016@gmail.com>
2024-01-03 17:21:17 -0300

    refactor: rename UserDTOMapper to UserControllerMapper

--- src/main/java/com/ocsoares/advancedcrudspringboot/infrastructure/controllers/user/CreateUserController.java
+++ src/main/java/com/ocsoares/advancedcrudspringboot/infrastructure/controllers/user/CreateUserController.java
@@ -5,7 +5,7 @@ import com.ocsoares.advancedcrudspringboot.domain.entity.UserDomainEntity;
 import com.ocsoares.advancedcrudspringboot.domain.exceptions.user.UserAlreadyExistsByEmailException;
 import com.ocsoares.advancedcrudspringboot.infrastructure.controllers.interfaces.IControllerWithArgument;
 import com.ocsoares.advancedcrudspringboot.infrastructure.controllers.user.dtos.CreateUserDTO;
-import com.ocsoares.advancedcrudspringboot.infrastructure.controllers.user.mapper.UserDTOMapper;
+import com.ocsoares.advancedcrudspringboot.infrastructure.controllers.user.mapper.UserControllerMapper;
 import com.ocsoares.advancedcrudspringboot.infrastructure.controllers.user.response.CreateUserResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CreateUserController implements IControllerWithArgument<CreateUserResponse, CreateUserDTO, Exception> {
     private final CreateUserUseCase createUserUseCase;
-    private final UserDTOMapper userDTOMapper;
+    private final UserControllerMapper userControllerMapper;
 
     @Override
     @PostMapping("user")
@@ -31,12 +31,12 @@ public class CreateUserController implements IControllerWithArgument<CreateUserR
         // Como o "CreateUserUseCase" usa apenas o Objeto de DOMÍNIO usado nas REGRAS de NEGÓCIO da
         // Aplicação, no caso o "UserDomainEntity", PRECISA CONVERTER esse "createUserDTO" para o
         // DOMÍNIO!!!
-        UserDomainEntity createUserDomain = this.userDTOMapper.toDomain(createUserDTO);
+        UserDomainEntity createUserDomain = this.userControllerMapper.toDomain(createUserDTO);
 
         UserDomainEntity createdUser = this.createUserUseCase.execute(createUserDomain);
 
         // O "createdUser" é um "UserDomainEntity", então PRECISA CONVERTER ele para o Retorno do Método,
         // que é "CreateUserResponse" !!!
-        return this.userDTOMapper.toResponse(createdUser);
+        return this.userControllerMapper.toResponse(createdUser);
     }
 }
--- src/main/java/com/ocsoares/advancedcrudspringboot/infrastructure/controllers/user/mapper/UserControllerMapper.java
+++ src/main/java/com/ocsoares/advancedcrudspringboot/infrastructure/controllers/user/mapper/UserControllerMapper.java
@@ -4,7 +4,7 @@ import com.ocsoares.advancedcrudspringboot.domain.entity.UserDomainEntity;
 import com.ocsoares.advancedcrudspringboot.infrastructure.controllers.user.dtos.CreateUserDTO;
 import com.ocsoares.advancedcrudspringboot.infrastructure.controllers.user.response.CreateUserResponse;
 
-public class UserDTOMapper {
+public class UserControllerMapper {
     public UserDomainEntity toDomain(CreateUserDTO createUserDTO) {
         return new UserDomainEntity(createUserDTO.name(), createUserDTO.email(), createUserDTO.password());
     }
