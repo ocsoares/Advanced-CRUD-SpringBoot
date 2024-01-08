96a2a2ebd83514a1d3157e62d78a7d626cab40b0
ocsoares <casoares016@gmail.com>
2024-01-07 21:29:45 -0300

    feat: add UserDetails type to findByEmail return

--- src/main/java/com/ocsoares/advancedcrudspringboot/infrastructure/persistence/repository/jpa/JpaUserRepository.java
+++ src/main/java/com/ocsoares/advancedcrudspringboot/infrastructure/persistence/repository/jpa/JpaUserRepository.java
@@ -1,12 +1,12 @@
 package com.ocsoares.advancedcrudspringboot.infrastructure.persistence.repository.jpa;
 
-import com.ocsoares.advancedcrudspringboot.domain.entity.UserDomainEntity;
 import com.ocsoares.advancedcrudspringboot.infrastructure.persistence.entity.UserPersistenceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface JpaUserRepository extends JpaRepository<UserPersistenceEntity, UUID> {
-    Optional<UserDomainEntity> findByEmail(String email);
+    Optional<UserDetails> findByEmail(String email);
 }
