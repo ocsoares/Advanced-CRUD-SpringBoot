10b658d5b98fb722cf60bf7e03005231b224441f
ocsoares <casoares016@gmail.com>
2024-01-09 16:19:32 -0300

    feat: add ErrorJWTVerificationException

--- src/main/java/com/ocsoares/advancedcrudspringboot/infrastructure/exceptions/HttpExceptionHandler.java
+++ src/main/java/com/ocsoares/advancedcrudspringboot/infrastructure/exceptions/HttpExceptionHandler.java
@@ -2,6 +2,7 @@ package com.ocsoares.advancedcrudspringboot.infrastructure.exceptions;
 
 import com.ocsoares.advancedcrudspringboot.domain.exceptions.response.MessageAndStatusCodeResponse;
 import com.ocsoares.advancedcrudspringboot.domain.exceptions.security.ErrorCreatingJWTException;
+import com.ocsoares.advancedcrudspringboot.domain.exceptions.security.ErrorJWTVerificationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -37,6 +38,16 @@ public class HttpExceptionHandler {
     ) {
         // Usei "var" aqui porque o Tipo do Retorno OBVIAMENTE vai ser "MessageAndStatusCodeResponse", mas digitar assim
         // ficaria MUITO Extenso sem necessidade!!!
+        var bodyResponse = new MessageAndStatusCodeResponse(
+                exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(bodyResponse);
+    }
+
+    @ExceptionHandler(ErrorJWTVerificationException.class)
+    public ResponseEntity<MessageAndStatusCodeResponse> handleErrorJWTVerificationException(
+            ErrorJWTVerificationException exception
+    ) {
         var bodyResponse = new MessageAndStatusCodeResponse(
                 exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
 
