386eedde2f46b1997f61d3421c20f42f432b558a
ocsoares <casoares016@gmail.com>
2024-01-20 12:56:52 -0300

    docs: add Swagger Bearer Authentication

--- src/main/java/com/ocsoares/advancedcrudspringboot/main/config/SwaggerConfig.java
+++ src/main/java/com/ocsoares/advancedcrudspringboot/main/config/SwaggerConfig.java
@@ -4,11 +4,14 @@ import com.ocsoares.advancedcrudspringboot.domain.exceptions.response.MessageAnd
 import io.swagger.v3.core.converter.AnnotatedType;
 import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.core.converter.ResolvedSchema;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.media.Content;
 import io.swagger.v3.oas.models.media.MediaType;
 import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,9 +21,11 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customAPI() {
         return new OpenAPI().info(new Info().title("Advanced Crud Spring Boot")
-                .version("1.0.0")
-                .description(
-                        "Uma API REST de CRUD avançada realizada com Spring Boot, Arquitetura Limpa, autenticação e testes."));
+                        .version("1.0.0")
+                        .description(
+                                "Uma API REST de CRUD avançada realizada com Spring Boot, Arquitetura Limpa, autenticação e testes."))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", this.securityScheme()));
     }
 
     // Padronizando as Responses para NÃO ter que ficar REPETINDO no Código de cada Documentação de Rota!!!
@@ -40,5 +45,9 @@ public class SwaggerConfig {
                                 .addApiResponse("500",
                                         new ApiResponse().description("Internal Server Error").content(content)
                                 )));
+    }
+
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP).bearerFormat("JWT").scheme("bearer");
     }
 }
