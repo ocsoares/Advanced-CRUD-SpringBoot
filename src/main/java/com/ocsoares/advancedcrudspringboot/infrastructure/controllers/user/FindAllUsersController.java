800bcb7cfbc61ab0917138ba40f26703a0b788b5
ocsoares <casoares016@gmail.com>
2024-01-20 13:53:10 -0300

    docs: add FindAllUsersController swagger documentation

--- src/main/java/com/ocsoares/advancedcrudspringboot/infrastructure/controllers/user/FindAllUsersController.java
+++ src/main/java/com/ocsoares/advancedcrudspringboot/infrastructure/controllers/user/FindAllUsersController.java
@@ -3,6 +3,9 @@ package com.ocsoares.advancedcrudspringboot.infrastructure.controllers.user;
 import com.ocsoares.advancedcrudspringboot.application.usecases.response.UserResponse;
 import com.ocsoares.advancedcrudspringboot.application.usecases.user.FindAllUsersUseCase;
 import com.ocsoares.advancedcrudspringboot.infrastructure.controllers.interfaces.IControllerWithoutArgument;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +20,10 @@ public class FindAllUsersController implements IControllerWithoutArgument<List<U
     private final FindAllUsersUseCase findAllUsersUseCase;
 
     @Override
+    @Operation(summary = "Find all users", tags = "User")
+    @ApiResponse(responseCode = "200")
+    @ApiResponse(responseCode = "403", content = @Content)
+    @ApiResponse(responseCode = "500")
     @GetMapping("user")
     @ResponseStatus(HttpStatus.OK)
     public List<UserResponse> handle() {
